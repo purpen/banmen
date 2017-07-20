40b19577a19f171b5f7835523745c26a40d2d91e
dong <609042238@qq.com>
2017-07-20 11:55:17 +0800

    个人中心

--- banmen/banmen/Modules/User/Controller/UserViewController.m
+++ banmen/banmen/Modules/User/Controller/UserViewController.m
@@ -8,8 +8,9 @@
 
 #import "UserViewController.h"
 #import "UserView.h"
+#import "UserModel.h"
 
-@interface UserViewController ()
+@interface UserViewController () <UserModelDelegate>
 
 @property(nonatomic, strong) UserView *userView;
 
@@ -19,7 +20,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UserModel *userModel = [[UserModel findAll] lastObject];
+    userModel.delegate = self;
+    [userModel netGetUserInfo];
     [self.view addSubview:self.userView];
+    [self.userView.updatePersonalInformationBtn addTarget:self action:@selector(updateUserInfo) forControlEvents:(UIControlEventTouchUpInside)];
+}
+
+-(void)updateUserInfo{
+    
+}
+
+-(void)update{
+    UserModel *userModel = [[UserModel findAll] lastObject];
+    self.userView.userModel = userModel;
 }
 
 -(UserView *)userView{
--- banmen/banmen/Modules/User/View/UserView.h
+++ banmen/banmen/Modules/User/View/UserView.h
@@ -7,10 +7,39 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "UserModel.h"
 
 @interface UserView : UIView
 
 @property(nonatomic, strong) UIView *topView;
+@property(nonatomic, strong) UIButton *updatePersonalInformationBtn;
 @property(nonatomic, strong) UIImageView *headImageView;
+@property(nonatomic, strong) UILabel *nameLabel;
+@property(nonatomic, strong) UIImageView *goImageView;
+@property(nonatomic, strong) UIView *lineView;
+@property(nonatomic, strong) UILabel *goodLabel;
+@property(nonatomic, strong) UILabel *goodCountLabel;
+@property(nonatomic, strong) UILabel *salesLabel;
+@property(nonatomic, strong) UILabel *salesCountLabel;
+@property(nonatomic, strong) UILabel *orderLabel;
+@property(nonatomic, strong) UILabel *orderCountLabel;
+@property(nonatomic, strong) UserModel *userModel;
+
+@property(nonatomic, strong) UIView *middleView;
+@property(nonatomic, strong) UIButton *aboutBtn;
+@property(nonatomic, strong) UIButton *adjustBtn;
+@property(nonatomic, strong) UIButton *welcomeBtn;
+@property(nonatomic, strong) UIView *onelineView;
+@property(nonatomic, strong) UIView *twolineView;
+@property(nonatomic, strong) UIView *threelineView;
+@property(nonatomic, strong) UIView *fourlineView;
+@property(nonatomic, strong) UILabel *aboutLabel;
+@property(nonatomic, strong) UILabel *adjustLabel;
+@property(nonatomic, strong) UILabel *welcomeLabel;
+@property(nonatomic, strong) UIImageView *onegoImageView;
+@property(nonatomic, strong) UIImageView *twogoImageView;
+@property(nonatomic, strong) UIImageView *threegoImageView;
+
+@property(nonatomic, strong) UIButton *logOutBtn;
 
 @end
