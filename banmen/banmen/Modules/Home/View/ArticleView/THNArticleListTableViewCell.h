49e92a0dd954778367d3504594eef73f95f6b81c
dong <609042238@qq.com>
2017-08-03 18:09:29 +0800

    重写商品详情

---
+++ banmen/banmen/Modules/Home/View/ArticleView/THNArticleListTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  THNArticleListTableViewCell.h
+//  banmen
+//
+//  Created by dong on 2017/8/3.
+//  Copyright © 2017年 banmen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "THNGoodsArticleModel.h"
+
+@interface THNArticleListTableViewCell : UICollectionViewCell
+
+@property (nonatomic, strong) UILabel *wordLabel;
+@property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) THNGoodsArticleModel *goodsArticleModel;
+
+@end
---
+++ banmen/banmen/Modules/Home/View/ArticleView/THNArticleListTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  THNArticleListTableViewCell.m
+//  banmen
+//
+//  Created by dong on 2017/8/3.
+//  Copyright © 2017年 banmen. All rights reserved.
+//
+
+#import "THNArticleListTableViewCell.h"
+
+@implementation THNArticleListTableViewCell
+
+@end
